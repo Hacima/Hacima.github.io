160079d060cad97d2c417a0cb5b5c721eac773b3
Micah J. Obal <micah_obal@yahoo.com>
2015-08-26 16:56:31 -0500

    Uploading some new things!

--- dropdown.js
+++ dropdown.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.dropDown').hover(function(){
-        $('.subMenu').css("visibility", "visible");
+        $('.subMenu', this).css("visibility", "visible");
     }, function() {
         $('.subMenu').css("visibility", "hidden");
     });
---
+++ pong.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var ballSize = 5,
+        ballSpeed = 1,
+        paddleLength = 40,
+        paddleWidth = 5;
+    
+    
+});
+
+function moveBall() {
+    $("").css(margin-top) 
+}
